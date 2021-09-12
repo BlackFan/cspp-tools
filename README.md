3edd3f44c3f617182cddd2a3df2b35816446dcf3
Sergey Bobrov <blackfan@ya.ru>
2021-09-12 09:16:08 +0400

    Add match_rules & prototype_checker

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# cspp-tools
-Client-Side Prototype Pollution Tools
+# Client-Side Prototype Pollution Tools
+
+## Match rules for Burp Software Version Reporter extension
+
+Match rules that passively detect vulnerable libraries even in minified JS code.
+
+**Rules:** [match_rules/match-rules.tab](/match_rules/match-rules.tab)  
+**Extension:** [Software Version Reporter](https://portswigger.net/bappstore/ae62baff8fa24150991bad5eaf6d4d38)
+
+![Screenshot at 08-56-42](https://user-images.githubusercontent.com/3295867/132972901-d60e742e-a4ad-4759-a079-4f96b870e4b1.png)
+
+## Prototype Checker
+
+JS script that highlights custom fields in prototypes and constructors that can be useful in exploiting Prototype Pollution.
+
+**Script:** [prototype_checker/prototype_checker.js](/prototype_checker/prototype_checker.js)  
+**Script Gadget Example:** TODO
+
+![Screenshot at 18-34-30](https://user-images.githubusercontent.com/3295867/132973030-42b61f1c-f25d-451c-a034-1788d2fb0ff9.png)
---
+++ prototype_checker/prototype_checker.js
@@ -0,0 +1,58 @@
+	def = []
+	def['Object'] = ["length", "name", "prototype", "assign", "getOwnPropertyDescriptor", "getOwnPropertyDescriptors", "getOwnPropertyNames", "getOwnPropertySymbols", "is", "preventExtensions", "seal", "create", "defineProperties", "defineProperty", "freeze", "getPrototypeOf", "setPrototypeOf", "isExtensible", "isFrozen", "isSealed", "keys", "entries", "fromEntries", "values"]
+	def['Object.prototype'] = ["constructor", "__defineGetter__", "__defineSetter__", "hasOwnProperty", "__lookupGetter__", "__lookupSetter__", "isPrototypeOf", "propertyIsEnumerable", "toString", "valueOf", "__proto__", "toLocaleString"]
+
+	def['String'] = ["length", "name", "prototype", "fromCharCode", "fromCodePoint", "raw"]
+	def['String.prototype'] = ["length", "constructor", "anchor", "big", "blink", "bold", "charAt", "charCodeAt", "codePointAt", "concat", "endsWith", "fontcolor", "fontsize", "fixed", "includes", "indexOf", "italics", "lastIndexOf", "link", "localeCompare", "match", "matchAll", "normalize", "padEnd", "padStart", "repeat", "replace", "search", "slice", "small", "split", "strike", "sub", "substr", "substring", "sup", "startsWith", "toString", "trim", "trimStart", "trimLeft", "trimEnd", "trimRight", "toLocaleLowerCase", "toLocaleUpperCase", "toLowerCase", "toUpperCase", "valueOf", "replaceAll"]
+
+	def['Number'] = ["length", "name", "prototype", "isFinite", "isInteger", "isNaN", "isSafeInteger", "parseFloat", "parseInt", "MAX_VALUE", "MIN_VALUE", "NaN", "NEGATIVE_INFINITY", "POSITIVE_INFINITY", "MAX_SAFE_INTEGER", "MIN_SAFE_INTEGER", "EPSILON"]
+	def['Number.prototype'] = ["constructor", "toExponential", "toFixed", "toPrecision", "toString", "valueOf", "toLocaleString"]
+
+	def['Array'] = ["length", "name", "prototype", "isArray", "from", "of"]
+	def['Array.prototype'] = ["length", "constructor", "concat", "copyWithin", "fill", "find", "findIndex", "lastIndexOf", "pop", "push", "reverse", "shift", "unshift", "slice", "sort", "splice", "includes", "indexOf", "join", "keys", "entries", "values", "forEach", "filter", "flat", "flatMap", "map", "every", "some", "reduce", "reduceRight", "toLocaleString", "toString"]
+
+	def['Function'] = ["length", "name", "prototype", "arguments", "caller"]
+	def['Function.prototype'] = ["length", "name", "arguments", "caller", "constructor", "apply", "bind", "call", "toString"]
+
+	def['Boolean'] = ["length", "name", "prototype"]
+	def['Boolean.prototype'] = ["constructor", "toString", "valueOf"]
+
+	check = {
+		'Object': Object, 
+		'Object.prototype': Object.prototype, 
+		'String': String, 
+		'String.prototype': String.prototype, 
+		'Number': Number, 
+		'Number.prototype': Number.prototype, 
+		'Array': Array, 
+		'Array.prototype': Array.prototype, 
+		'Function': Function, 
+		'Function.prototype': Function.prototype, 
+		'Boolean': Boolean, 
+		'Boolean.prototype': Boolean.prototype
+	}
+
+	function getType(obj) { 
+		if(obj == null) return 'null'
+		var funcNameRegex = /function (.{1,})\(/
+		var results = (funcNameRegex).exec((obj).constructor.toString())
+		return (results && results.length > 1) ? results[1] : ""
+	}
+
+	function checkProperties(obj, objType, path) {
+		var filtered = Object.getOwnPropertyNames(obj).filter(value => (!(def[objType] ?? []).includes(value)) && !(objType=="String" && /\d/.test(value)))
+		filtered.forEach(k => {
+			kpath = `${path}["${k}"]`
+			type = getType(obj[k])
+			if(!['Function'].includes(type))
+				console.log(`%c${kpath} ${type}`, 'background: #222; color: #bada55;')
+			else
+				console.log(`${kpath} ${type}`)
+			if(obj[k] != null)
+			checkProperties(obj[k], type, `${kpath}`)
+		})
+	}
+
+	Object.keys(check).forEach(key => {
+		checkProperties(check[key], key, key)
+	})
